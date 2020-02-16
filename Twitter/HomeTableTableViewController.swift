4748ddca88dc84ee4475c7d98126bc9c067e63c9
garyouyang <49287999+garyouyang@users.noreply.github.com>
2020-02-15 23:24:19 -0800

    twitter pt2

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -86,6 +86,9 @@ class HomeTableTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
@@ -98,6 +101,10 @@ class HomeTableTableViewController: UITableViewController {
         tableView.refreshControl = myRefreshControl
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadMoreTweet()
+    }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
